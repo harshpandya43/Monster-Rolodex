5f3c9b585564c3a92cec6d853ea7264f18f6c794
Harsh Pandya <pandyaharsh43@gmail.com>
2019-08-13 12:33:26 -0700

    Updates

---
+++ precache-manifest.3f79b9f0b918b36308acda59404cc28a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff9dc71f88c1022600eb3ffa5d7566f",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "28ea820425e96d16cd58",
+    "url": "/Monster-Rolodex/static/css/main.465aa689.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/Monster-Rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "28ea820425e96d16cd58",
+    "url": "/Monster-Rolodex/static/js/main.f9431915.chunk.js"
+  },
+  {
+    "revision": "2b9873f68be40fb50022",
+    "url": "/Monster-Rolodex/static/js/runtime~main.7532dd55.js"
+  }
+]);
